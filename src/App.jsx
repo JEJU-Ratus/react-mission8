b703601b03bc7c94a274854dc830697c80df9d4f
jema <smh1141@naver.com>
2026-07-25 14:51:53 +0900

    feat: 즐겨찾기 필터링 버튼 추가 및 상태 관리 옵션 추가

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         const matchedCategory = category === "all" || item.category === category;
         return matchedCategory && matchedKeyword;
       }),
-    [keyword, category],
+    [keyword, category, favoriteIds, favoriteOnly],
   );
 
   const handleToggleFavorite = _id => {
@@ -35,6 +35,13 @@ function App() {
         </div>
         <div className="card p-2">
           <CategoryFilter category={category} onCategoryChange={setCategory} />
+          <button
+            className={`btn mt-2 ${favoriteOnly ? "btn-secondary active" : "btn-primary"}`}
+            onClick={() => setFavoriteOnly(prev => !prev)}
+          >
+            {/* 현재 보기 상태에서 반대로 보여야 하기 때문에 true일때 전체항목 보기 활성화, false일때는 즐겨찾기 보기 활성화 */}
+            {favoriteOnly ? "전체항목 보기" : "즐겨찾기만 보기"}
+          </button>
         </div>
         <div className="card p-2">
           <StudyList
